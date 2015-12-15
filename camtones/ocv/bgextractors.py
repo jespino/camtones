152ff5960e634e401363ae8dffcb672ab53422cd
Jesús Espino <jespinog@gmail.com>
2015-12-15 21:46:01 +0100

    Add KNN to supported background substractors

--- camtones/ocv/bgextractors.py
+++ camtones/ocv/bgextractors.py
@@ -7,7 +7,10 @@ class BackgroundExtractor:
     def __init__(self, extractor_type="MOG2"):
         if extractor_type == "MOG2":
             self._extractor = cv2.createBackgroundSubtractorMOG2()
+        elif extractor_type == "KNN":
+            self._extractor = cv2.createBackgroundSubtractorKNN()
 
     def apply(self, frame):
         result_frame = self._extractor.apply(frame.frame)
+
         return Frame(result_frame)
