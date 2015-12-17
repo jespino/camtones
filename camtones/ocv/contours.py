c268e1fdc03ed249a9eaf38c55c207d1ef558e48
Jesús Espino <jespinog@gmail.com>
2015-12-17 15:41:58 +0100

    Adding width/height properties to Contour class

--- camtones/ocv/contours.py
+++ camtones/ocv/contours.py
@@ -8,6 +8,14 @@ class Contour:
         self.area = cv2.contourArea(self.contour)
 
     @property
+    def width(self):
+        return self.w
+
+    @property
+    def height(self):
+        return self.h
+
+    @property
     def point1(self):
         return (self.x, self.y)
 
